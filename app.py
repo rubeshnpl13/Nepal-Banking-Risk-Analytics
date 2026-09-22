ec07c1b61375df0ab457ffc3a88bdce6f3726487
rubeshnpl13 <nepalnishanta29@gmail.com>
2026-09-22 21:49:09 +0545

    Git username check

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from src.data_loader import (
     load_combined_data,
     load_summary_data,
 )
-
+#check github profile
 st.set_page_config(page_title="Nepal Banking Risk Analytics Dashboard", layout="wide")
 
 st.title("Nepal Banking Risk Analytics Dashboard")
